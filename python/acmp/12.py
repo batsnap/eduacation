19e4edf9edaa463fdf8028521450d85bb6cda0f4
batsnap <46847292+batsnap@users.noreply.github.com>
2019-02-11 19:02:15 +0300

    new

--- python/acmp/12.py
+++ python/acmp/12.py
@@ -1,9 +1,6 @@
 n=int(input())
-s=0
+a=[]
 for i in range(n):
-    x,y,x1,y1,x2,y2,x3,y3,x4,y4=map(int,input().split())
-    if((x>=x1)and(x<=x3)and(y>=y1)and(y<=y3)):
-        s+=1
-    elif ((x>=x2)and(x<=x4)and(y>=y2)and(y<=y4)):
-        s+=1
+    a=list(map(int,input().split()))
+
 print(s)
