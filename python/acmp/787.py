835e7b2c52caedabe4279c9418516707da2e4f89
Batyr <batkur@gmail.com>
2019-02-12 11:18:37 +0300

    New

--- python/acmp/787.py
+++ python/acmp/787.py
@@ -13,11 +13,3 @@ for i in range(len(a)):
     if k2>=a[i]:
         k2=a[i]
         c2=i
-if c>n//2 and c2<n//2:
-    print(a[n//2],)
-elif c>n//2 and c2>n//2:
-    print(a[n//2]-1)
-elif c<n//2 and c2>n//2:
-    print(a[n//2])
-elif c>n//2 and c2>n//2:
-    print(a[n])
