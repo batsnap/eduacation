b3db54baf808b5894bad992bec16725754cd19fe
Batyr <batkur@gmail.com>
2019-02-14 15:32:28 +0300

    Add

--- python/acmp/787.py
+++ python/acmp/787.py
@@ -1,15 +1,29 @@
 n=int(input())
-a=list(map(int, input().split()))
-c=0
-c2=0
-k=a[0]
-k2=1000
-for i in range(len(a)):
-    if k<a[i]:
-        k=a[i]
-        c=i
+a=[[]]*n
+for i in range(n):
+   a[i]=[0]*(n-i)
 
-for i in range(len(a)):
-    if k2>=a[i]:
-        k2=a[i]
-        c2=i
+b=list(map(int, input().split()))
+for i in range (n):
+   a[0][i]=b[i]
+for i in range (1,n):
+   for j in range(1,n-i+1):
+      if i%2==1:
+         if a[i-1][j]>a[i-1][j-1]:
+            a[i][j-1]=a[i-1][j]
+         elif a[i-1][j]<a[i-1][j-1]:
+            a[i][j-1]=a[i-1][j-1]
+         else:
+            a[i][j-1]=a[i-1][j]
+        
+      elif i%2==0:
+         if a[i-1][j]>a[i-1][j-1]:
+            a[i][j-1]=a[i-1][j-1]
+         elif a[i-1][j]<a[i-1][j-1]:
+            a[i][j-1]=a[i-1][j]
+         else:
+            a[i][j-1]=a[i-1][j]
+        
+print(a[n-1][0])
+         
+
