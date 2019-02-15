01882e7b6cac6c6785e90d230c8a0692b2c2c7c2
Batyr <batkur@gmail.com>
2019-02-15 09:30:44 +0300

    New

--- python/testy/generator.py
+++ python/testy/generator.py
@@ -1,8 +1,15 @@
 from random import randint,uniform
 x=int(input())
 y=int(input())
+c=str()
+l=open('output.txt','w')
+l=l.write('test')
+l.write(str(5))
 b= [[randint(0, 10) for i in range(x)] for j in range(y)]
 for i in range(y):
     for j in range (x):
-        print(b[i][j],end=' ')
-    print()
+        c=str(b[i][j])
+        print(c)
+        l.write(c)
+        l.write(' ')
+    l.write('\n')
