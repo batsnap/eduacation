9d3dc6b9f703c9774d255c4a72d2fb3811ee5854
batkur@gmail.com <batkur@gmail.com>
2019-02-20 01:06:00 +0300

    add

--- python/acmp/17.py
+++ python/acmp/17.py
@@ -1,2 +1,18 @@
-n=int(input())
+#n=int(input())
+b=[]
+k=0
+l=[]
+v=''
+stroka=''
+stroka2=''
 a=list(map(int,input().split()))
+c=len(a)-1
+for i in range (2,c):
+    if c%i==0:
+        b.append(i)
+for i in range(len(b)): 
+    for j in range(c//b[i]):
+        stroka=stroka+str(a[j])+' '
+    l.append(stroka)
+    stroka=''
+print(l)
