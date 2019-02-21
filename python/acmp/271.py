df11f695dfcc04d9e44514d2efe51ff6f52ee481
Batyr <batkur@gmail.com>
2019-02-21 13:26:03 +0300

    New

--- python/acmp/271.py
+++ python/acmp/271.py
@@ -4,18 +4,14 @@ fib2 = 1
 k=-1
 i = 0
 a=[1,1]
-while i < n - 2:
+fib_sum=0
+while i < 1200000000 - 2 and fib_sum<n and fib_sum!=n:
     fib_sum = fib1 + fib2
     fib1 = fib2
     fib2 = fib_sum
     i = i + 1
-    a.append(fib_sum)
-for i in range(len(a)):
-    if a[i]==n:
-        k=i
-if k>0:
-    print(1)
-    print(k+1)
+if fib_sum==n:
+   print(1)
+   print(i+2)
 else:
-    print(0)
-
+   print(0)
---
+++ python/acmp/79.py
@@ -0,0 +1,4 @@
+a,b=map(int,input().split())
+a=a%100
+b=b%100
+print((a**b)%10)
