bbfc83e00e41a861f7bc5ff31346029fa8afbb7c
Batyr <batkur@gmail.com>
2019-02-22 00:24:50 +0300

    Add

---
+++ python/acmp/457.py
@@ -0,0 +1,25 @@
+def kaprikar(x:int):
+   m=[]
+   l=0
+   r=''
+   a=x//1000
+   b=(x%1000)//100
+   c=(x%100)//10
+   d=x%10
+   m.append(a)
+   m.append(b)
+   m.append(c)
+   m.append(d)
+   m.sort()
+   x=int(str(m[0])+str(m[1])+str(m[2])+str(m[3]))
+   m.reverse()
+   a=int(str(m[0])+str(m[1])+str(m[2])+str(m[3]))
+   return a-x
+n=int(input())
+i=0
+while kaprikar(n)!=6174:
+   x=kaprikar(n)
+   i+=1
+print(6174)
+print(i)
+kakka
