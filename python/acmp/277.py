a1865a9abddb4f99f849c5a1a24f5e26e6cdb844
batkur@gmail.com <batkur@gmail.com>
2019-02-28 02:26:39 +0300

    adda

--- python/acmp/277.py
+++ python/acmp/277.py
@@ -2,37 +2,59 @@ a,b,c=map(int,input().split())
 k=''
 if a>0:
    k=k+str(a)
-   if b>0:
+   if b==1:
+      k=k+'+x'
+   elif b==-1:
+      k=k+'-x'
+   elif b>0:
       k=k+'+'+str(b)+'x'
    elif b<0:
-      k=k++str(b)+'x'
-   if c>0:
+      k=k+str(b)+'x'
+   if c==1:
+      k=k+'+y'
+   elif c==-1:
+      k=k+'-y'
+   elif c>0:
       k=k+'+'+str(c)+'y'
    elif c<0:
       k=k+str(c)+'y'
 
 elif a<0:
    k=k+str(a)
-   if b>0:
+   if b==1:
+      k=k+'+x'
+   elif b==-1:
+      k=k+'-x'
+   elif b>0:
       k=k+'+'+str(b)+'x'
    elif b<0:
       k=k++str(b)+'x'
-   if c>0:
+   if c==1:
+      k=k+'+y'
+   elif c==-1:
+      k=k+'-y'
+   elif c>0:
       k=k+'+'+str(c)+'y'
    elif c<0:
       k=k+str(c)+'y'
 elif a==0:
-   if b>0:
+   if b==1:
+      k=k+'x'
+   elif b==-1:
+      k=k+'-x'
+   elif b>0:
       k=k+str(b)+'x'
    elif b<0:
-      k=k++str(b)+'x'
-   if c>0:
+      k=k+str(b)+'x'
+   if c==1:
+      k=k+'+y'
+   elif c==-1:
+      k=k+'-y'
+   elif c>0:
       k=k+'+'+str(c)+'y'
    elif c<0:
       k=k+str(c)+'y'
-print(k)
-
-   
-
-   
-   
+if k=='':
+   print(0)
+else:
+   print(k)
