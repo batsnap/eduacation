41325caabd2857ff129b70aa45ee0f163c5407e5
Batyr <batkur@gmail.com>
2019-03-16 16:43:03 +0300

    Add

--- python/Repetiror/25/var9.py
+++ python/Repetiror/25/var9.py
@@ -1,9 +1,9 @@
 a=[]
+k=0
 n=5
 for i in range(0,n):
-	a.append(int(input())
-for i in range(n):
-	for j in range(1,n):
-		if a[i]%13==0 or a[j]%13==0:
+	a.append(int(input()))
+for i in range(n-1):
+		if a[i]%13==0 or a[i+1]%13==0:
 			k+=1
 print(k)
