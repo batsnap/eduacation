94966826af69d9aae3f0c8583dcb4b889148e96a
batkur@gmail.com <batkur@gmail.com>
2019-04-25 18:47:00 +0300

    add

--- python/ui/test_1.py
+++ python/ui/test_1.py
@@ -10,40 +10,38 @@ with con:
   cur = con.cursor()    
   cur.execute("SELECT * FROM PrePri")
   rows = cur.fetchall()
-
+a=[]
+b=[]
+for row in rows:
+  a.append(row[0])
+  b.append(row[1])
 c=randint(0,184)
 v=[]
 x=0
 r=0
 def start(sender):
-	global x,r,v,f,k
+	global x,r
 	label = sender.superview['label1']
 	label2 = sender.superview['label2']
 	if x==0:
 		x=1
-		v=rows[c]
-    f=v[0]
-    k=v[1]
-		label.text=f
+		label.text=a[c]
 	else:
 		x=0
 		r=0
 		label.text='Слова'
 		label2.text='Ошибки'
 def but(sender):
-	global c,r,v,f,k
+	global c,r,x
 	t=sender.title
 	label = sender.superview['label1']
 	label2 = sender.superview['label2']
 	if x==0:
 		pass
 	else:
-		if t==k:
+		if t==b[c]:
 			c=randint(0,184)
-      v=rows[c]
-      f=v[0]
-      k=v[1]
-			label.text=f
+			label.text=a[c]
 		else:
 			r+=1
 			label2.text='Ошибки'+str(r)
