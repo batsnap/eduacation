6ae27640a06fd727531bca46d98777ec1cadf928
batkur <batkur@gmail.com>
2019-05-16 21:21:22 +0300

    add

--- programming/python/database/inputdb.py
+++ programming/python/database/inputdb.py
@@ -1,10 +1,23 @@
 # -*- coding: utf-8 -*-
 import sqlite3
-conn = sqlite3.connect('./rez.sqlite')
+conn = sqlite3.connect('./my.sqlite')
 c = conn.cursor()
-c.execute('CREATE TABLE rez (Time REAL ,osh INTEGER)')
-
-   
+try:
+   c.execute('CREATE TABLE PrePri (Words TEXT,Letters TEXT)')
+except:
+   pass
+l=[]
+a=''
+while a!='0':
+	try:
+		if a!=0:
+			a,b=map(str,input().split())
+			l.append(a)
+			l.append(b)
+			c.execute('INSERT INTO PrePri VALUES(?,?)',l)
+			l=[]
+	except:
+		pass
 conn.commit()
 c.close()
 conn.close()
