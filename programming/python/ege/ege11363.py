c975ea4d88d8992877f701581636f5c9614baae1
batkur <batkur@gmail.com>
2019-06-10 15:22:36 +0300

    reshuege

--- programming/python/ege/ege11363.py
+++ programming/python/ege/ege11363.py
@@ -1,4 +1,17 @@
 n=int(input())
+maxsum=0
+raznmin=0
 for i in range(n):
 	a,b=map(int,input().split())
-	
+	if a>b:
+		maxsum+=a
+	else:
+		maxsum+=b
+	if abs(a-b)%3>0 and abs(a-b)<10001:
+		raznmin=abs(a-b)
+if maxsum%3==0:
+	if raznmin>10001:
+		maxsum=0
+	else:
+		maxsum-=raznmin
+print(maxsum)
--- programming/python/ege/ege15937.py
+++ programming/python/ege/ege15937.py
@@ -1,17 +1,18 @@
 n=int(input())
 n6=n3nechet=chet=nechet=m6max=n3nechetmax=chetmax=nechetmax=0
+s1=s2=s3=0
 for i in range(n):
 	x=int(input())
 	if x%6==0 and x>n6max:
 		n6max=x
 		n6+=1
-	elif x%3==0 and x%2!==0 and x>n3nechetmax:
+	elif x%3==0 and x%2!=0 and x>n3nechetmax:
 		n3nechet+=1
 		n3nechetmax=x
 	elif x%3!=0 and x%2!=0 and x>nechetmax:
 		nechet+=1
 		nechetmax=x
-	elif x%3!=0 and x%2===0 and x>chetmax:
+	elif x%3!=0 and x%2==0 and x>chetmax:
 		chetmax=x
 		chet+=1
 if n6>0 and nechet>0:
@@ -20,4 +21,14 @@ if n3nechet>0 and chet>0:
 	s2=n3nechetmax+chetmax
 if n6>0 and n3nechet>0:
 	s3=n6max+n3nechetmax
-if s1!=0 or s2!=
+c=[s1,s2,s3]
+k=c.index(max(c))
+if max(c)!=0:
+	if k==0:
+		print(n6max,nechetmax)
+	elif k==1:
+		print(n3nechetmax,chetmax)
+	elif k==2:
+		print(n6max,n3nechetmax)
+else:
+	print(0)
