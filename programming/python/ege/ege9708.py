ee35d2a99180615de1b1163b838792d3ae24ae3e
batkur <batkur@gmail.com>
2019-06-27 17:34:26 +0300

    add

--- programming/python/ege/ege9708.py
+++ programming/python/ege/ege9708.py
@@ -4,12 +4,14 @@ for i in range(n):
 	x=int(input())
 	if x%14==0 and x>n14:
 		n14=x
-	elif x%7==0 and x>n7:
-		n7=x
-	elif x%2==0 and x>n2:
-		n2=x
-	elif x>maxn:
-			maxn=x
+	else: 
+		if x>maxn:
+    		maxn=x
+		if x%7==0 and x>n7:
+			n7=x
+		elif x%2==0 and x>n2:
+			n2=x
+	
 if n2*n7>n14*maxn and n2*n7>n14*n2 and n2*n7>n14*n7:
 	print(n2*n7)
 elif n2*n7<n14*maxn and n14*maxn>n14*n2 and n14*maxn>n14*n7:
