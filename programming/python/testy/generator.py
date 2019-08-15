fdd1a3e5e507d3c02265fcfa74abd9fe1e665dbd
batkur <batkur@gmail.com>
2019-08-15 22:57:45 +0300

    add

--- programming/python/testy/generator.py
+++ programming/python/testy/generator.py
@@ -2,7 +2,7 @@ from random import randint,uniform
 x=int(input())
 y=int(input())
 c=str()
-b= [randint(0, 10) for i in range(x)] for j in range(y)]
+b=[[randint(0, 10) for i in range(x)] for j in range(y)]
 for i in range(y):
     for j in range (x):
         c=str(b[i][j])
