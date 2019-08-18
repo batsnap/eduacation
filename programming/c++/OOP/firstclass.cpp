42f73635718e1bf49a114fd804a45566312855d6
batkur <batkur@gmail.com>
2019-08-18 17:56:00 +0300

    add

---
+++ programming/c++/OOP/firstclass.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+class myclass{
+    public:
+        myclass(){
+            cout<<"hello world"<<endl;
+        }
+};
+int main(){
+    myclass a;
+    return 0;
+}
