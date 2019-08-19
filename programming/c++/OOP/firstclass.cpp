ed7006fb2215a21a806a85224a5962d578ddb32f
batkur <batkur@gmail.com>
2019-08-20 00:36:24 +0300

    add

--- programming/c++/OOP/firstclass.cpp
+++ programming/c++/OOP/firstclass.cpp
@@ -6,8 +6,18 @@ class myclass{
         myclass(){
             cout<<"hello world"<<endl;
         }
+        void getName(string x){
+            name=x;
+        }
+        string outName(){
+            return name;
+        }
+    private:
+        string name;
 };
 int main(){
     myclass a;
+    a.getName("batyr");
+    cout<<a.outName()<<endl;
     return 0;
 }
