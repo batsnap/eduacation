f363be4a0e4f54e6f0c6f44ad38d7f0a823aa274
batkur <batkur@gmail.com>
2019-08-27 11:27:25 +0300

    add

---
+++ programming/python/UsefulProgramms/ChangeSystemFromTO.py
@@ -0,0 +1,18 @@
+def changeFromTo(x,sis1,sis2):
+    num=['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F']
+    k=len(x)-1
+    s=0
+    itog=''
+    for i in range(len(x)):
+        s+=num.index(x[i])*(sis1**k)
+        k-=1
+    while s>0:
+        if s%sis2<10:
+            itog+=num[num.index(str(s%sis2))]
+            s//=sis2
+        else:
+            itog+=num[s%sis2]
+            s//=sis2
+    print(itog[::-1])
+
+changeFromTo('1AC6',16,15)
