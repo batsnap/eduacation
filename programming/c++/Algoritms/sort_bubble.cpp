1abd66a09662f57b2f872d44ff3bba537b4befdf
batsnap <batkur@gmail.com>
2019-09-08 09:33:28 +0300

    add

--- programming/c++/Algoritms/sort_bubble.cpp
+++ programming/c++/Algoritms/sort_bubble.cpp
@@ -2,19 +2,9 @@
 #include <cstdlib>
 #include <ctime>
 using namespace std;
-
-int main(){
-    srand(time(NULL));
-    int n,s;
-    n=-1;
-    while (n<=0){
-        cin>>n;
-    }
-    int a[n];
-    for(int i=0; i<n; i++){
-        a[i]=rand()%100;
-    }
-
+void bubble_sort(int a[],int n)
+{
+    int s;
     for(int i=0;i<(n-1);i++){
         for(int j=0;j<(n-1-i);j++){
             if(a[j]>a[j+1]){
@@ -25,10 +15,29 @@ int main(){
         }
     }
 
-    for(int i=0; i<n; i++){
-    cout<<a[i]<<endl;
-    }
+}
 
+int main(){
+    srand(time(NULL));
+    int n,s;
+    n=-1;
+    while (n<=0)
+    {
+        cin>>n;
+    }
+    int a[n];
+    for(int i=0; i<n; i++) 
+    {
+        a[i]=rand()%100;
+        cout<<a[i]<<" ";
+    }
+    bubble_sort(a,n);
+    cout<<endl;
+    for(int i=0; i<n; i++) 
+    {
+        cout<<a[i]<<" ";
+    }
+    cout<<endl;
     return 0;
 
 }
