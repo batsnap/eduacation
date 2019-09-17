dc3ae8636808fa2b2e291925ae7fa1467c055eb4
batsnap <batkur@gmail.com>
2019-09-17 12:38:33 +0300

    add

--- programming/python/testy/matrix.py
+++ programming/python/testy/matrix.py
@@ -1,3 +1,4 @@
+from random import randint
 a=[]
 for i in range (5):
    for j in range(5):
@@ -8,3 +9,7 @@ for i in range (5):
       a[i].append(0)
 for i in range(5):
    print(a[i])
+
+
+n=int(input())
+b=[[randint(-100,100) for i in range(n)] for j in range(n)]
